91bbb1b9a794eceae16583eaa183e3de9e7a2dae
roshanvernekar3 <your-email@example.com>
2026-07-26 00:59:44 +0530

    Initial commit

---
+++ Basics/Armstrong.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isArmstrong(self, n: int) -> bool:
+        nod = len(str(n))
+        num = n
+        total = 0
+        while num > 0:
+            ld = num % 10
+            total = total + (ld ** nod)
+            num = num // 10
+        return total == n
