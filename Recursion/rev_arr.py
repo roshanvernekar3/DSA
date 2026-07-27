6b8654c5aa203969cc8483c44e7ca5d5e81426bd
roshanvernekar3 <your-email@example.com>
2026-07-28 00:43:18 +0530

    Reverse an array using reccursion

---
+++ Recursion/rev_arr.py
@@ -0,0 +1,9 @@
+n = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+def func(arr, left, right):
+    if left >= right:
+        return
+    arr[left], arr[right] = arr[right], arr[left]
+    func(arr, left + 1, right - 1)
+func(n, 0, len(n) - 1)
+print("Reversed array:", n)
