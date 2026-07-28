e971a0812363f68c4d96d774ee4e5e49ce9bbd96
roshanvernekar3 <your-email@example.com>
2026-07-29 01:37:55 +0530

    Simple example to under selection sort

---
+++ Sorting/SelectionSort.py
@@ -0,0 +1,16 @@
+n = [5, 7, 8, 4, 1, 6, 9, 2]
+
+def SelectionSort(arr):
+    length = len(arr)
+    for i in range(0, length):
+        min_index = i
+        for j in range(i + 1, length):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        # Indented inside the outer loop
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+# Calling the function
+sorted_arr = SelectionSort(n)
+print("Output:", sorted_arr)
