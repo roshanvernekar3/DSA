c7158fbf220db19d7c84d3640cc905377fe31cf3
roshanvernekar3 <your-email@example.com>
2026-08-03 23:08:54 +0530

    Logic of Quick Sort

--- Sorting/QuickSort.py
+++ Sorting/QuickSort.py
@@ -4,6 +4,7 @@ class Solution:
         if low < high:
             # Partition the array and get the pivot index
             p_index = self.partition(arr, low, high)
+
             # Recursively sort elements before and after partition
             self.quickSort(arr, low, p_index - 1)
             self.quickSort(arr, p_index + 1, high)
@@ -16,8 +17,10 @@ class Solution:
         while i < j:
             while i <= high - 1 and arr[i] <= pivot:
                 i += 1
+
             while j >= low + 1 and arr[j] > pivot:
                 j -= 1
+
             if i < j:
                 arr[i], arr[j] = arr[j], arr[i]
 
